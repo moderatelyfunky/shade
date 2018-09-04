dd2e35979844c1b3bbdd442cddc63c4752bcbb72
smartalecvt <alec@alecjulien.com>
2018-09-04 10:33:18 -0400

    Now works on Pi!

--- tktest9.py
+++ tktest9.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import time
 
+
 class ButtonTimer:
     timerOn = False
     timerCount = 0.0
@@ -13,23 +14,30 @@ class ButtonTimer:
 def doSomethingElse(event):
     print(event.widget)
 
+
 def timingStart(event, t: ButtonTimer):
-    if str(event.widget) == ".!button11":
+    if str(event.widget) == ".btnSmallCircle1":
         print("Small Button Pressed")
         if t.timerOn == False:
+            # if the timer is off, start it and get the current time
             t.timerOn = True
             t.timerCount = time.time()
 
 
 def timingStop(event, t: ButtonTimer):
-    if str(event.widget) == ".!button11":
+    if str(event.widget) == ".btnSmallCircle1":
         print("Small Button Released")
         if t.timerOn == True:
+            # if the timer is on, stop it and get the count
             t.timerOn = False
             t.timerCount = time.time() - t.timerCount
             print(str(t.timerCount))
 
+
 class Rect:
+    # (start_x, start_y) --> upper left corner
+    # (end_x, end_y) --> lower right corner
+    # the id is so we can easily delete from the canvas
     start_x = 0
     start_y = 0
     end_x = 0
@@ -44,22 +52,25 @@ class Rect:
         self.id = id
 
 
-def drawMe(event):
-    event.x
-
-
 def getStartCoords(event, r1):
-    canvasDraw.option_clear()
+    # figure out where the user has clicked on the canvas
+    # store that point in the rect object
+    # and then delete the previous rectangle
+    # canvasDraw.option_clear()
     r1.start_x = event.x
     r1.start_y = event.y
-    print(str(r1.start_x) + " " + str(r1.start_y))
+    # print(str(r1.start_x) + " " + str(r1.start_y))
     canvasDraw.delete(r1.id)
 
 
 def getEndCoords(event, r1):
+    # when the user releases the mouse, get the coords there
+    # and store them as the end point in the rect object
+    # Then draw a rectangle spanning the start to end pts
+    # and store the id of the new rectangle in the rect object
     r1.end_x = event.x
     r1.end_y = event.y
-    print(str(r1.end_x) + " " + str(r1.end_y))
+    # print(str(r1.end_x) + " " + str(r1.end_y))
     i = canvasDraw.create_rectangle(r1.start_x, r1.start_y, r1.end_x, r1.end_y, fill="black")
     r1.id = i
 
@@ -81,69 +92,65 @@ arrowUp=tk.PhotoImage(file="images/arrow-up.gif")
 arrowDown=tk.PhotoImage(file="images/arrow-down.gif")
 roundButton = tk.PhotoImage(file="images/round-button.gif")
 smallRoundButton = tk.PhotoImage(file="images/small-round-button.gif")
-drawingArea = tk.PhotoImage(file="images/rect-rev.gif")
 
 canvasDraw = tk.Canvas(app, width=195, height=419, borderwidth=1)
 canvasDraw.grid(row=1, column=1, rowspan=5, ipadx=3, ipady=3)
-canvasDraw.bind('<B1-Motion>', drawMe)
 canvasDraw.bind('<ButtonPress-1>', lambda event: getStartCoords(event, r1))
 canvasDraw.bind('<ButtonRelease-1>', lambda event: getEndCoords(event, r1))
 
-btnCircle1 = tk.Button(app, image=roundButton, borderwidth=0)
+btnCircle1 = tk.Button(app, name="btnCircle1", image=roundButton, borderwidth=0)
 btnCircle1.grid(row=1, column=3, rowspan=2, columnspan=2, ipadx=3, ipady=3)
 btnCircle1.bind('<ButtonRelease-1>', doSomethingElse)
 btnCircle1.bind('<ButtonPress-1>', doSomethingElse)
-btnCircle2 = tk.Button(app, image=roundButton, borderwidth=0)
+btnCircle2 = tk.Button(app, name="btnCircle2", image=roundButton, borderwidth=0)
 btnCircle2.grid(row=4, column=3, rowspan=2, columnspan=2, ipadx=3, ipady=3)
 btnCircle2.bind('<ButtonRelease-1>', doSomethingElse)
 btnCircle2.bind('<ButtonPress-1>', doSomethingElse)
-btnTopShadeUp = tk.Button(app, image=arrowLeft, borderwidth=0)
+btnTopShadeUp = tk.Button(app, name="btnTopShadeUp", image=arrowLeft, borderwidth=0)
 btnTopShadeUp.grid(row=3, column=3, ipadx=3, ipady=3)
 btnTopShadeUp.bind('<ButtonRelease-1>', doSomethingElse)
 btnTopShadeUp.bind('<ButtonPress-1>', doSomethingElse)
-btnTopShadeDown = tk.Button(app, image=arrowRight, borderwidth=0)
+btnTopShadeDown = tk.Button(app, name="btnTopShadeDown", image=arrowRight, borderwidth=0)
 btnTopShadeDown.grid(row=3, column=4, ipadx=3, ipady=3)
 btnTopShadeDown.bind('<ButtonRelease-1>', doSomethingElse)
 btnTopShadeDown.bind('<ButtonPress-1>', doSomethingElse)
-btnLeftShadeLeft = tk.Button(app, image=arrowUp, borderwidth=0)
+btnLeftShadeLeft = tk.Button(app, name="btnLeftShadeLeft", image=arrowUp, borderwidth=0)
 btnLeftShadeLeft.grid(row=1, column=5, ipadx=3, ipady=3)
 btnLeftShadeLeft.bind('<ButtonRelease-1>', doSomethingElse)
 btnLeftShadeLeft.bind('<ButtonPress-1>', doSomethingElse)
-btnLeftShadeRight = tk.Button(app, image=arrowDown, borderwidth=0)
+btnLeftShadeRight = tk.Button(app, name="btnLeftShadeRight", image=arrowDown, borderwidth=0)
 btnLeftShadeRight.grid(row=2, column=5, ipadx=3, ipady=3)
 btnLeftShadeRight.bind('<ButtonRelease-1>', doSomethingElse)
 btnLeftShadeRight.bind('<ButtonPress-1>', doSomethingElse)
-btnRightShadeLeft = tk.Button(app, image=arrowUp, borderwidth=0)
+btnRightShadeLeft = tk.Button(app, name="btnRightShadLeft", image=arrowUp, borderwidth=0)
 btnRightShadeLeft.grid(row=4, column=5, ipadx=3, ipady=3)
 btnRightShadeLeft.bind('<ButtonRelease-1>', doSomethingElse)
 btnRightShadeLeft.bind('<ButtonPress-1>', doSomethingElse)
-btnRightShadeRight = tk.Button(app, image=arrowDown, borderwidth=0)
+btnRightShadeRight = tk.Button(app, name="btnRightShadeRight", image=arrowDown, borderwidth=0)
 btnRightShadeRight.grid(row=5, column=5, ipadx=3, ipady=3)
 btnRightShadeRight.bind('<ButtonRelease-1>', doSomethingElse)
 btnRightShadeRight.bind('<ButtonPress-1>', doSomethingElse)
-btnBottomShadeUp = tk.Button(app, image=arrowLeft, borderwidth=0)
+btnBottomShadeUp = tk.Button(app, name="btnBottomShadeUp", image=arrowLeft, borderwidth=0)
 btnBottomShadeUp.grid(row=3, column=6, ipadx=3, ipady=3)
 btnBottomShadeUp.bind('<ButtonRelease-1>', doSomethingElse)
 btnBottomShadeUp.bind('<ButtonPress-1>', doSomethingElse)
-btnBottomShadeDown = tk.Button(app, image=arrowRight, borderwidth=0)
+btnBottomShadeDown = tk.Button(app, name="btnBottomShadeDown", image=arrowRight, borderwidth=0)
 btnBottomShadeDown.grid(row=3, column=7, ipadx=3, ipady=3)
 btnBottomShadeDown.bind('<ButtonRelease-1>', doSomethingElse)
 btnBottomShadeDown.bind('<ButtonPress-1>', doSomethingElse)
-btnSmallCircle1 = tk.Button(app, image=smallRoundButton, borderwidth=0)
+btnSmallCircle1 = tk.Button(app, name="btnSmallCircle1", image=smallRoundButton, borderwidth=0)
 btnSmallCircle1.grid(row=1, column=6, ipadx=3, ipady=3)
-#btnSmallCircle1.bind('<ButtonRelease-1>', doSomethingElse)
-#btnSmallCircle1.bind('<ButtonPress-1>', doSomethingElse)
 btnSmallCircle1.bind('<ButtonPress-1>', lambda event: timingStart(event, timer1))
 btnSmallCircle1.bind('<ButtonRelease-1>', lambda event: timingStop(event, timer1))
-btnSmallCircle2 = tk.Button(app, image=smallRoundButton, borderwidth=0)
+btnSmallCircle2 = tk.Button(app, name="btnSmallCircle2", image=smallRoundButton, borderwidth=0)
 btnSmallCircle2.grid(row=2, column=6, ipadx=3, ipady=3)
 btnSmallCircle2.bind('<ButtonRelease-1>', doSomethingElse)
 btnSmallCircle2.bind('<ButtonPress-1>', doSomethingElse)
-btnSmallCircle3 = tk.Button(app, image=smallRoundButton, borderwidth=0)
+btnSmallCircle3 = tk.Button(app, name="btnSmallCircle3", image=smallRoundButton, borderwidth=0)
 btnSmallCircle3.grid(row=4, column=6, ipadx=3, ipady=3)
 btnSmallCircle3.bind('<ButtonRelease-1>', doSomethingElse)
 btnSmallCircle3.bind('<ButtonPress-1>', doSomethingElse)
-btnSmallCircle4 = tk.Button(app, image=smallRoundButton, borderwidth=0)
+btnSmallCircle4 = tk.Button(app, name="btnSmallCircle4", image=smallRoundButton, borderwidth=0)
 btnSmallCircle4.grid(row=5, column=6, ipadx=3, ipady=3)
 btnSmallCircle4.bind('<ButtonRelease-1>', doSomethingElse)
 btnSmallCircle4.bind('<ButtonPress-1>', doSomethingElse)
