b3c1accb6f39ce373525bdb6e949f554ce1caec8
Joe Englert <englertj@biotek.com>
2018-09-12 20:10:36 -0400

    start of objectifying the UI code

--- middle.py
+++ middle.py
@@ -21,20 +21,24 @@ class Middle():
         self.config.read(self.iniFileName)
         self.gotPi = self.config.get('config', 'gotPi')
         if (self.gotPi == 1):
-            print('Pi is being present')
+            print('Running Pifull')
             return self.gotPi
         else:
-            print('No Pi is here.')
+            print('Running Piless')
             return self.gotPi
 
     def gotoPreset(self, event, presetNo):
         if (self.piHere == '1'):
             self.gUnit.gotoPreset(event, presetNo)
+            
+        print('Finished going to preset ' + str(presetNo))  
 
     def writePreset(self, event, presetNo):
         if (self.piHere == '1'):
             self.gUnit.writePreset(event, presetNo)
-
+            
+        print('Preset ' + str(presetNo) + ' stored ')
+    
     def stop(self, event, shade):
         if (self.piHere == '1'):
             if (shade == 'left'):
@@ -45,6 +49,7 @@ class Middle():
                 self.gUnit.topShade.motor.stop(event)
             elif (shade == 'bot'):
                 self.gUnit.botShade.motor.stop(event)
+        print('Stopped ' + shade + ' shade')
 
     def move(self, event, shade, direction):
         if (self.piHere == '1'):
@@ -56,3 +61,4 @@ class Middle():
                 self.gUnit.topShade.motor.move(event, direction)
             elif (shade == 'bot'):
                 self.gUnit.botShade.motor.move(event, direction)
+        print('Moving ' + shade + ' shade in direction ' + str(direction))
--- view.py
+++ view.py
@@ -5,13 +5,76 @@ import time
 import middle
 mid = middle.Middle()
 
-class ButtonWrapper(tk.Button):
-    #jge - for additional button properties
-    def __init__(self, btnType, presetNo, *args, **kwargs):
-        self.type = btnType
-        self.presetNo = presetNo
+class BigButton(tk.Button):
+    #jge - extend tk.Button
+    def __init__(self, presetNo, *args, **kwargs):
         tk.Button.__init__(self, *args, **kwargs)
-   
+        self.presetNo = presetNo
+        self.configure(image=bigRoundButton, borderwidth=0)
+        self.grid(rowspan=2, columnspan=2, ipadx=3, ipady=3)
+        self.bind('<ButtonPress-1>', lambda event: self.btnPress(event))
+        self.bind('<ButtonRelease-1>', lambda event: self.btnRelease(event))
+    
+    def btnPress(self, event):
+        event.widget.configure(image=bigRoundButtonGreenGlow)
+        mid.gotoPreset(event, self.presetNo)  
+
+    def btnRelease(self, event):
+        event.widget.configure(image=bigRoundButton)
+
+class ManualButton(tk.Button):
+    #jge - extend tk.Button
+    def __init__(self, shade, direction, image, *args, **kwargs):
+        tk.Button.__init__(self, *args, **kwargs)
+        self.shade = shade
+        self.direction = direction
+        #jge - <ButtonPress-1> is the left mouse button
+        self.configure(image=image, borderwidth=0)
+        self.bind('<ButtonRelease-1>', lambda event: mid.stop(event, self.shade))
+        self.bind('<ButtonPress-1>', lambda event: mid.move(event, self.shade, self.direction))
+        self.grid(ipadx=3, ipady=3)
+        
+class PresetButton(tk.Button):
+    #jge - extend tk.Button
+    def __init__(self, presetNo, *args, **kwargs):
+        tk.Button.__init__(self, *args, **kwargs)
+        self.presetNo = presetNo
+        self.btnTimer = ButtonTimer(False, 0.0)
+        self.bind('<ButtonPress-1>', lambda event: self.btnPress(event))
+        self.bind('<ButtonRelease-1>', lambda event: self.btnRelease(event))
+        self.configure(image=smallRoundButton, borderwidth=0)
+        self.grid(columnspan=2, ipadx=3, ipady=3)
+
+    def drawGreenPreset(self, event) :
+        event.widget.configure(image=smallButtonGreenGlow)
+    
+    def drawNormalPreset(self, event) :
+        event.widget.configure(image=smallButtonGreenGlow)
+        app.after(500, self.drawGreenPreset(event))
+
+    def btnPress(self, event):
+        event.widget.configure(image=smallButtonGreenGlow)
+        
+        if self.btnTimer.timerOn == False:
+            # if the timer is off, start it and get the current time
+            self.btnTimer.timerOn = True
+            self.btnTimer.timerCount = time.time()
+
+    def btnRelease(self, event):
+        event.widget.configure(image=smallRoundButton)
+        if self.btnTimer.timerOn == True:
+            # if the timer is on, stop it and get the count
+            self.btnTimer.timerOn = False
+            self.btnTimer.timerCount = time.time() - self.btnTimer.timerCount
+
+            #jge - save the preset if they held it down
+            if (self.btnTimer.timerCount > 2):
+                app.after(500, self.drawNormalPreset(event))
+                mid.writePreset(event, self.presetNo)
+                #jge - flash to indicate successful save
+            else:
+                mid.gotoPreset(event, self.presetNo)  
+
 class ButtonTimer:
     timerOn = False
     timerCount = 0.0
@@ -19,42 +82,6 @@ class ButtonTimer:
     def __init__(self, timerOn, timerCount):
         self.timerOn = timerOn
         self.timerCount = timerCount
-
-def drawGreenPreset(event) :
-    event.widget.configure(image=smallButtonGlow)
-  
-def drawNormalPreset(event) :
-    event.widget.configure(image=smallButtonGlow)
-    app.after(500, drawGreenPreset(event))
-
-def timingStart(event, t: ButtonTimer):
-    theType = str(event.widget.type)
-
-    if theType  == "preset":
-        event.widget.configure(image=smallButtonGlow)
-        
-        if t.timerOn == False:
-            # if the timer is off, start it and get the current time
-            t.timerOn = True
-            t.timerCount = time.time()
-
-def timingStop(event, t: ButtonTimer, presetNo):
-    theType = str(event.widget.type)
-    
-    if theType  == "preset":
-        event.widget.configure(image=smallRoundButton)
-        if t.timerOn == True:
-            # if the timer is on, stop it and get the count
-            t.timerOn = False
-            t.timerCount = time.time() - t.timerCount
-
-            #jge - save the preset if they held it down
-            if (t.timerCount > 2):
-                app.after(500, drawNormalPreset(event))
-                mid.writePreset(event, presetNo)
-                #jge - flash to indicate successful save
-            else:
-                mid.gotoPreset(event, presetNo)         
 
 class Rect:
     # (start_x, start_y) --> upper left corner
@@ -73,28 +100,29 @@ class Rect:
         self.end_y = end_y
         self.id = id
 
-def getStartCoords(event, r1):
-    # figure out where the user has clicked on the canvas
-    # store that point in the rect object
-    # and then delete the previous rectangle
-    # canvasDraw.option_clear()
-    r1.start_x = event.x
-    r1.start_y = event.y
-    canvasDraw.delete(r1.id)
+    def getStartCoords(self, event):
+        # figure out where the user has clicked on the canvas
+        # store that point in the rect object
+        # and then delete the previous rectangle
+        # canvasDraw.option_clear()
+        self.start_x = event.x
+        self.start_y = event.y
+        canvasDraw.delete(self.id)
+        print('Starting freehand draw')
 
-def getEndCoords(event, r1):
-    # when the user releases the mouse, get the coords there
-    # and store them as the end point in the rect object
-    # Then draw a rectangle spanning the start to end pts
-    # and store the id of the new rectangle in the rect object
-    r1.end_x = event.x
-    r1.end_y = event.y
-    i = canvasDraw.create_rectangle(r1.start_x, r1.start_y, r1.end_x, r1.end_y, fill="black")
-    r1.id = i
+    def getEndCoords(self, event):
+        # when the user releases the mouse, get the coords there
+        # and store them as the end point in the rect object
+        # Then draw a rectangle spanning the start to end pts
+        # and store the id of the new rectangle in the rect object
+        self.end_x = event.x
+        self.end_y = event.y
+        i = canvasDraw.create_rectangle(self.start_x, self.start_y, self.end_x, self.end_y, fill="black")
+        self.id = i
+        print('Stopping freehand draw')
 
-r1 = Rect(0, 0, 0, 0, 0)
-timer1 = ButtonTimer(False, 0.0)
-
+#################################
+#jge - form settings
 app = tk.Tk()
 app.geometry("800x480")
 #app.wm_attributes('-type', 'dock')
@@ -103,99 +131,74 @@ app.option_add("Button.Background", "black")
 app.option_add("Button.Foreground", "white")
 app.resizable(0, 0)
 #app.configure(background='white')
+#################################
 
 arrowLeft=tk.PhotoImage(file="images/arrow-left.gif")
 arrowRight=tk.PhotoImage(file="images/arrow-right.gif")
 arrowUp=tk.PhotoImage(file="images/arrow-up.gif")
 arrowDown=tk.PhotoImage(file="images/arrow-down.gif")
-roundButton = tk.PhotoImage(file="images/big-button-off.gif")
-roundButtonGlow = tk.PhotoImage(file="images/big-button-on.gif")
+bigRoundButton = tk.PhotoImage(file="images/big-button-off.gif")
+bigRoundButtonGreenGlow = tk.PhotoImage(file="images/big-button-on.gif")
 smallRoundButton = tk.PhotoImage(file="images/small-button-off.gif")
-smallButtonGlow = tk.PhotoImage(file="images/small-button-on.gif")
+smallButtonGreenGlow = tk.PhotoImage(file="images/small-button-on.gif")
 
 canvasDraw = tk.Canvas(app, width=195, height=419, borderwidth=1)
 canvasDraw.grid(row=1, column=1, rowspan=5, ipadx=3, ipady=3)
-canvasDraw.bind('<ButtonPress-1>', lambda event: getStartCoords(event, r1))
-canvasDraw.bind('<ButtonRelease-1>', lambda event: getEndCoords(event, r1))
+canvasDraw.bind('<ButtonPress-1>', lambda event: freehandArea.getStartCoords(event))
+canvasDraw.bind('<ButtonRelease-1>', lambda event: freehandArea.getEndCoords(event))
 
+freehandArea = Rect(0, 0, 0, 0, 0)
 ########################
 #jge - two button section
-btnCircle1 = tk.Button(app, name="btnCircle1", image=roundButton, borderwidth=0)
-btnCircle1.grid(row=1, column=3, rowspan=2, columnspan=2, ipadx=3, ipady=3)
-btnCircle1.bind('<ButtonPress-1>', lambda event: mid.gotoPreset(event, 5))
+btnOpenWide = BigButton(5)
+btnOpenWide.grid(row=1, column=3)
 
-btnCircle2 = tk.Button(app, name="btnCircle2", image=roundButton, borderwidth=0)
-btnCircle2.grid(row=4, column=3, rowspan=2, columnspan=2, ipadx=3, ipady=3)
-btnCircle2.bind('<ButtonPress-1>', lambda event: mid.gotoPreset(event, 6))
+btnCloseCenter = BigButton(6)
+btnCloseCenter.grid(row=4, column=3)
 #jge - end two button
 ########################
 
 ########################
 #jge - manual movers
-btnTopShadeUp = tk.Button(app, name="btnTopShadeUp", image=arrowRight, borderwidth=0)
-btnTopShadeUp.grid(row=3, column=7, ipadx=3, ipady=3)
-btnTopShadeUp.bind('<ButtonRelease-1>', lambda event: mid.stop(event, 'top'))
-btnTopShadeUp.bind('<ButtonPress-1>', lambda event: mid.move(event, 'top', 1))
+btnTopShadeUp = ManualButton('top', 1, arrowRight)
+btnTopShadeUp.grid(row=3, column=7)
 
-btnTopShadeDown = tk.Button(app, name="btnTopShadeDown", image=arrowLeft, borderwidth=0)
-btnTopShadeDown.grid(row=3, column=6, ipadx=3, ipady=3)
-btnTopShadeDown.bind('<ButtonRelease-1>', lambda event: mid.stop(event, 'top'))
-btnTopShadeDown.bind('<ButtonPress-1>', lambda event: mid.move(event, 'top', 0))
+btnTopShadeDown = ManualButton('top', 0, arrowLeft)
+btnTopShadeDown.grid(row=3, column=6)
 
-btnLeftShadeLeft = tk.Button(app, name="btnLeftShadeLeft", image=arrowUp, borderwidth=0)
-btnLeftShadeLeft.grid(row=1, column=5, ipadx=3, ipady=3)
-btnLeftShadeLeft.bind('<ButtonRelease-1>', lambda event: mid.stop(event, 'left'))
-btnLeftShadeLeft.bind('<ButtonPress-1>', lambda event: mid.move(event, 'left', 1))
+btnLeftShadeLeft = ManualButton('left', 1, arrowUp)
+btnLeftShadeLeft.grid(row=1, column=5)
 
-btnLeftShadeRight = tk.Button(app, name="btnLeftShadeRight", image=arrowDown, borderwidth=0)
-btnLeftShadeRight.grid(row=2, column=5, ipadx=3, ipady=3)
-btnLeftShadeRight.bind('<ButtonRelease-1>', lambda event: mid.stop(event, 'left'))
-btnLeftShadeRight.bind('<ButtonPress-1>', lambda event: mid.move(event, 'left', 0))
+btnLeftShadeRight = ManualButton('left', 0, arrowDown)
+btnLeftShadeRight.grid(row=2, column=5)
 
-btnRightShadeLeft = tk.Button(app, name="btnRightShadLeft", image=arrowUp, borderwidth=0)
-btnRightShadeLeft.grid(row=4, column=5, ipadx=3, ipady=3)
-btnRightShadeLeft.bind('<ButtonRelease-1>', lambda event: mid.stop(event, 'right'))
-btnRightShadeLeft.bind('<ButtonPress-1>', lambda event: mid.move(event, 'right', 0))
+btnRightShadeLeft = ManualButton('right', 0, arrowUp)
+btnRightShadeLeft.grid(row=4, column=5)
 
-btnRightShadeRight = tk.Button(app, name="btnRightShadeRight", image=arrowDown, borderwidth=0)
-btnRightShadeRight.grid(row=5, column=5, ipadx=3, ipady=3)
-btnRightShadeRight.bind('<ButtonRelease-1>', lambda event: mid.stop(event, 'right'))
-btnRightShadeRight.bind('<ButtonPress-1>', lambda event: mid.move(event, 'right', 1))
+btnRightShadeRight = ManualButton('right', 1, arrowDown)
+btnRightShadeRight.grid(row=5, column=5)
 
-btnBottomShadeUp = tk.Button(app, name="btnBottomShadeUp", image=arrowRight, borderwidth=0)
-btnBottomShadeUp.grid(row=3, column=4, ipadx=3, ipady=3)
-btnBottomShadeUp.bind('<ButtonRelease-1>', lambda event: mid.stop(event, 'bot'))
-btnBottomShadeUp.bind('<ButtonPress-1>', lambda event: mid.move(event, 'bot', 1))
+btnBottomShadeUp = ManualButton('bot', 1, arrowRight)
+btnBottomShadeUp.grid(row=3, column=4)
 
-btnBottomShadeDown = tk.Button(app, name="btnBottomShadeDown", image=arrowLeft, borderwidth=0)
-btnBottomShadeDown.grid(row=3, column=3, ipadx=3, ipady=3)
-btnBottomShadeDown.bind('<ButtonRelease-1>', lambda event: mid.stop(event, 'bot'))
-btnBottomShadeDown.bind('<ButtonPress-1>', lambda event: mid.move(event, 'bot', 0))
+btnBottomShadeDown = ManualButton('bot', 0, arrowLeft)
+btnBottomShadeDown.grid(row=3, column=3)
 #jge - end manual movers
 ########################
 
 ########################
 #jge - preset section
-btnSmallCircle1 = ButtonWrapper('preset', 1,  app, name="btnSmallCircle1", image=smallRoundButton, borderwidth=0)
-btnSmallCircle1.grid(row=1, column=6, columnspan=2, ipadx=3, ipady=3)
-btnSmallCircle1.bind('<ButtonPress-1>', lambda event: timingStart(event, timer1))
-btnSmallCircle1.bind('<ButtonRelease-1>', lambda event: timingStop(event, timer1, 1))
+btnPreset1 = PresetButton(1, name="btnPreset1")
+btnPreset1.grid(row=1, column=6)
 
-btnSmallCircle2 = ButtonWrapper('preset', 2,  app, name="btnSmallCircle2", image=smallRoundButton, borderwidth=0)
-btnSmallCircle2.grid(row=2, column=6, columnspan=2, ipadx=3, ipady=3)
-btnSmallCircle2.bind('<ButtonPress-1>', lambda event: timingStart(event, timer1))
-btnSmallCircle2.bind('<ButtonRelease-1>', lambda event: timingStop(event, timer1, 2))
+btnPreset2 = PresetButton(2, name="btnPreset2")
+btnPreset2.grid(row=2, column=6)
 
-btnSmallCircle3 = ButtonWrapper('preset', 3,app, name="btnSmallCircle3", image=smallRoundButton, borderwidth=0)
-btnSmallCircle3.grid(row=4, column=6, columnspan=2, ipadx=3, ipady=3)
-btnSmallCircle3.bind('<ButtonPress-1>', lambda event: timingStart(event, timer1))
-btnSmallCircle3.bind('<ButtonRelease-1>', lambda event: timingStop(event, timer1, 3))
+btnPreset3 = PresetButton(3, name="btnPreset3")
+btnPreset3.grid(row=4, column=6)
 
-#btnSmallCircle4 = tk.Button(app, name="btnSmallCircle4", image=smallRoundButton, borderwidth=0)
-btnSmallCircle4 = ButtonWrapper('preset', 4, app, name="btnSmallCircle4", image=smallRoundButton, borderwidth=0)
-btnSmallCircle4.grid(row=5, column=6, columnspan=2, ipadx=3, ipady=3)
-btnSmallCircle4.bind('<ButtonPress-1>', lambda event: timingStart(event, timer1))
-btnSmallCircle4.bind('<ButtonRelease-1>', lambda event: timingStop(event, timer1, 4))
+btnPreset4 = PresetButton(4, name="btnPreset4")
+btnPreset4.grid(row=5, column=6)
 #jge - end preset
 ########################
 
